cb36c22a8452d9f97e52b24f950d545abc256060
zivse <zivseker88@gmail.com>
2021-11-30 23:16:21 +0200

    add default tests

---
+++ src/test/java/bgu/spl/mics/example/services/ExampleEventHandlerServiceTest.java
@@ -0,0 +1,17 @@
+package bgu.spl.mics.example.services;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ExampleEventHandlerServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
